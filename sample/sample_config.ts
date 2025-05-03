0cde18cbee864f76f1c530401411f02d90ca11ef
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2025-05-03 19:48:25 +0900

    DSL。

--- sample/sample_config.ts
+++ sample/sample_config.ts
@@ -1,4 +1,3 @@
-"use strict";
 /*
  * Copyright 2025 agwlvssainokuni
  *
@@ -14,9 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = require("path");
-const date_fns_1 = require("date-fns");
+
+import {basename} from 'path'
+import {addDays, lightFormat} from 'date-fns'
+
 archive_one_to_one('一対一アーカイブ', {
     basedir: `${__dirname}/0file`,
     collector: collect_by_generation({
@@ -24,32 +24,35 @@ archive_one_to_one('一対一アーカイブ', {
         extra_cond: (a) => !!a.match(/_(\d{14})\.txt$/),
     }),
     to_dir: `${__dirname}/1arch`,
-    arcname: (a, _) => `${(0, path_1.basename)(a, '.txt')}.zip`,
-});
+    arcname: (a, _) => `${basename(a, '.txt')}.zip`,
+})
+
 archive_many_to_one('集約アーカイブ', {
     basedir: `${__dirname}/0file`,
     collector: collect_by_generation({
         pattern: ['aggregate_*.txt'],
     }),
     to_dir: `${__dirname}/1arch`,
-    arcname: (time) => `aggregate_${(0, date_fns_1.lightFormat)(time, 'yyyyMMddHHmmss')}.zip`,
-});
+    arcname: (time) => `aggregate_${lightFormat(time, 'yyyyMMddHHmmss')}.zip`,
+})
+
 backup_file('退避テスト', {
     basedir: `${__dirname}/1arch`,
     collector: collect_by_threshold({
         pattern: ['foreach_*.zip', 'aggregate_*.zip'],
         extra_cond: (a) => !!a.match(/_(\d{14})\.zip$/),
         slicer: (a) => a.match(/_(\d{14})\.zip$/)?.[1],
-        threshold: (time) => (0, date_fns_1.lightFormat)((0, date_fns_1.addDays)(time, -1), 'yyyyMMddHHmmss'),
+        threshold: (time) => lightFormat(addDays(time, -1), 'yyyyMMddHHmmss'),
     }),
     to_dir: `${__dirname}/2back`,
-});
+})
+
 cleanup_file('削除テスト', {
     basedir: `${__dirname}/2back`,
     collector: collect_by_threshold({
         pattern: ['foreach_*.zip', 'aggregate_*.zip'],
         extra_cond: (a) => !!a.match(/_(\d{14})\.zip$/),
         slicer: (a) => a.match(/_(\d{14})\.zip$/)?.[1],
-        threshold: (time) => (0, date_fns_1.lightFormat)((0, date_fns_1.addDays)(time, -2), 'yyyyMMddHHmmss'),
+        threshold: (time) => lightFormat(addDays(time, -2), 'yyyyMMddHHmmss'),
     }),
-});
+})
